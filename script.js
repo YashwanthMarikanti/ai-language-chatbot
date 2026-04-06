a8af7db40faeb931fcee40b836b56fc6a8e4fa6e
Yashwanth Marikanti <yashwanth2416@gmail.com>
2026-04-06 22:55:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let lang = document.getElementById("language").value;
 document.getElementById("chat").innerHTML +=
 "<p><b>You:</b> " + input + "</p>";
 
+try {
+
 const response = await fetch("https://translate.argosopentech.com/translate", {
 method: "POST",
 headers: {
@@ -21,8 +23,18 @@ format: "text"
 
 const data = await response.json();
 
+console.log("API response:", data);   // DEBUG LINE
+
 document.getElementById("chat").innerHTML +=
 "<p><b>Bot:</b> " + data.translatedText + "</p>";
 
-document.getElementById("userInput").value = "";
+} catch (error) {
+
+console.log("Error:", error);   // DEBUG ERROR
+
+document.getElementById("chat").innerHTML +=
+"<p><b>Bot:</b> Translation error</p>";
+
+}
+
 }
