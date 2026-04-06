76abc9fbf8ce7314889614913911d103582b784f
Yashwanth Marikanti <yashwanth2416@gmail.com>
2026-04-06 22:59:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
-async function sendMessage() {
+async function sendMessage(){
 
 let input = document.getElementById("userInput").value;
 let lang = document.getElementById("language").value;
+let chat = document.getElementById("chat");
 
-document.getElementById("chat").innerHTML +=
-"<p><b>You:</b> " + input + "</p>";
+chat.innerHTML += "<p><b>You:</b> "+input+"</p>";
 
 try {
 
-const response = await fetch("https://translate.argosopentech.com/translate", {
-method: "POST",
-headers: {
-"Content-Type": "application/json"
+const response = await fetch(
+"https://libretranslate.de/translate",
+{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
 },
 body: JSON.stringify({
 q: input,
 source: "auto",
-target: lang,
-format: "text"
+target: lang
 })
 });
 
 const data = await response.json();
 
-console.log("API response:", data);   // DEBUG LINE
+console.log(data);
 
-document.getElementById("chat").innerHTML +=
-"<p><b>Bot:</b> " + data.translatedText + "</p>";
+chat.innerHTML += "<p><b>Bot:</b> "+data.translatedText+"</p>";
 
-} catch (error) {
+} catch(error){
 
-console.log("Error:", error);   // DEBUG ERROR
-
-document.getElementById("chat").innerHTML +=
-"<p><b>Bot:</b> Translation error</p>";
+console.log(error);
+chat.innerHTML += "<p><b>Bot:</b> Error translating</p>";
 
 }
 
